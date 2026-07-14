3356d73d0d361b284785ec4ae586b0f408b20174
Valar Studio <valarstudiomx@gmail.com>
2026-07-14 04:24:06 -0600

    Actualización
    Commit api.js
    Update index.html
    Commit style.css

---
+++ api.js
@@ -0,0 +1,37 @@
+const sheets_url = "https://script.google.com/macros/s/AKfycbyEkSY7FvTRugukqgXdbFTZWIMkAgYvDPtBwkE1A7pvLX2qK84YzV0h4nzL0FNS-kqb/exec";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const userProfile = document.getElementById('userProfile');
+	const loginLink = document.getElementById('loginLink');
+	const pointsDisplay = document.getElementById('pointsDisplay');
+	const currentUser = localStorage.getItem('valarUser');
+
+	if (currentUser) {
+		loginLink.style.display = 'none';
+		userProfile.style.display = 'flex';
+		pointsDisplay.textContent = "Cargando...";
+		const cleanedUser = currentUser.replace(/^@/, "").toLowerCase().trim();
+		fetch(`${sheets_url}?user=${encodeURIComponent(cleanedUser)}`)
+			.then(response => {
+				if (!response.ok) throw new Error("Error");
+				return response.json();
+			})
+			.then(data => {
+				if (data.success) {
+					pointsDisplay.textContent = `${data.points} Puntos`;
+				} else {
+					loginLink.style.display = 'block';
+					userProfile.style.display = 'none';
+					localStorage.removeItem('valarUser');
+				}
+			})
+			.catch(error => {
+				console.error("Error de red:", error);
+				pointsDisplay.textContent = "Error de red";
+			});
+
+	} else {
+		loginLink.style.display = 'block';
+		userProfile.style.display = 'none';
+	}
+});
